5a4ab2085aabbcd064c3e0c20a8728972034ca17
Sherlen124 <95984208+Sherlen124@users.noreply.github.com>
2021-12-17 16:12:06 +0300

    Add files via upload

---
+++ OpenCV_4.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <fstream>
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/stitching.hpp"
+
+using namespace std;
+using namespace cv;
+
+// Определим режим сшивания как панорома
+Stitcher::Mode mode = Stitcher::PANORAMA;
+
+// Массив для картинок
+vector<Mat> images;
+
+int main(int argc, char* argv[])
+{
+    // Получить все изображения, которые должны быть
+    // сшиты как аргументы из командной строки
+    for (int i = 1; i < argc; ++i)
+    {
+        Mat images = imread(argv[i]);
+        if (images.empty())
+        {
+            cout << "Can't read image '" << argv[i] << "'\n";
+            cin.get();
+            return -1;
+        }
+        images.push_back(img);
+    }
+   
+    // Определяем объект для хранения сшитого изображения
+    Mat pano;
+
+    // Создание объекта класса Stitcher с режимом панорома
+    Ptr<Stitcher> stitcher = Stitcher::create(Stitcher::PANORAMA);
+
+    // Команда для сшивания всех изображений, присутствующих в массиве изображений
+    Stitcher::Status status = stitcher->stitch(images, pano);
+    if (status != Stitcher::OK)
+    {
+        // Проверяем, не удалось ли склеить изображения
+        // статус в порядке, если изображения успешно прикреплены
+        cout << "Can't stitch images\n";
+        return -1;
+    }
+
+    namedWindow("Panorama_image");
+    imshow("Panorama_image", pano);
+    // Сохраняем новое изображение, сшитое из заданных
+    imwrite("Panorama_image.jpg", pano);
+    waitKey(0);
+    
+    return 0;
+
+}
